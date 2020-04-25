d10d8abdd6e38695a585d0146bb4c29ac2dc4401
César Soto Valero <cesarsotovalero@gmail.com>
2020-04-25 11:14:44 +0200

    Refactor picocli

--- src/main/java/launchers/PanktiLauncher.java
+++ src/main/java/launchers/PanktiLauncher.java
@@ -1,77 +1,108 @@
 package launchers;
 
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
+
 import picocli.CommandLine;
 import processors.FirstMethodProcessor;
 import processors.MethodProcessor;
 import spoon.MavenLauncher;
 import spoon.reflect.CtModel;
-import spoon.reflect.declaration.CtMethod;
-import spoon.reflect.declaration.CtPackage;
 import spoon.reflect.declaration.CtType;
 
-@CommandLine.Command(name = "java -jar target/<pankti-version-jar-with-dependencies.jar>",
-        description = "pankti converts application traces to tests",
-        usageHelpWidth = 100)
-public class PanktiLauncher {
-    // Accept project path from CLI
-    @CommandLine.Parameters(paramLabel = "PROJECT PATH", description = "Path to a Maven project")
-    String projectPath;
+@CommandLine.Command(
+    name = "java -jar target/<pankti-version-jar-with-dependencies.jar>",
+    description = "pankti converts application traces to tests",
+    usageHelpWidth = 100)
+public class PanktiLauncher implements Callable<Integer> {
 
-    @CommandLine.Option(names = {"-h", "--help "}, usageHelp = true, description = "exit after usage help")
-    private boolean usageHelpRequested;
+    @CommandLine.Parameters(
+        paramLabel = "PATH",
+        description = "Path of the Maven project")
+    private Path projectPath;
 
-    public void setProjectPath(String projectPath) {
-        this.projectPath = projectPath;
-    }
-
-    public String getProjectPath() {
-        return projectPath;
-    }
+    @CommandLine.Option(
+        names = {"-h", "--help"},
+        description = "Display help/usage.",
+        help = true)
+    private boolean help = false;
 
     public static void main(String[] args) {
-        PanktiLauncher panktiLauncher = new PanktiLauncher();
-        CommandLine commandLine = new CommandLine(panktiLauncher);
-        // Accept project path or -h only
-        try {
-            commandLine.parseArgs(args);
-            if (commandLine.isUsageHelpRequested()) {
-                commandLine.usage(System.out);
-                return;
-            }
-        } catch (Exception e) {
-            commandLine.usage(System.out);
-            return;
+        int exitCode = new CommandLine(new PanktiLauncher()).execute(args);
+        System.exit(exitCode);
+    }
+
+    public PanktiLauncher(final Path projectPath, final boolean help) {
+        this.projectPath = projectPath;
+        this.help = help;
+    }
+
+    public PanktiLauncher() {
+    }
+
+    @Override
+    public Integer call() {
+
+        if (help) {
+            System.out.println("Pankti version: " + "1.0-SNAPSHOT");
+            return 1;
         }
 
-        panktiLauncher.setProjectPath(args[0]);
-        System.out.println("Processing project at " + panktiLauncher.getProjectPath());
+        final String path = this.projectPath.toString();
+        final String name = this.projectPath.getFileName().toString();
 
-        MavenLauncher launcher = new MavenLauncher(panktiLauncher.getProjectPath(), MavenLauncher.SOURCE_TYPE.APP_SOURCE);
-        System.out.println("POM found at " + launcher.getPomFile().getPath());
-        launcher.getEnvironment().setAutoImports(true);
-        launcher.getEnvironment().setCommentEnabled(true);
+        System.out.println("Processing project: " + name);
+
+        MavenLauncher launcher = getMavenLauncher(path, name);
+        System.out.println("POM found at: " + launcher.getPomFile().getPath());
 
         // Build Spoon model
-        launcher.buildModel();
-        CtModel model = launcher.getModel();
+        CtModel model = buildSpoonModel(launcher);
 
-        // List all classes of model
-        int numberOfMethodsInProject = 0;
-        for (CtType<?> s : model.getAllTypes()) numberOfMethodsInProject += s.getMethods().size();
-
-        System.out.println("Total number of methods: " + numberOfMethodsInProject);
+        // List all methods of model
+        System.out.println("Total number of methods: " + countMethods(model));
 
         // Apply processor to model
+        applyProcessor(model);
+
+        // Save model in spooned/
+        launcher.prettyprint();
+
+        return 0;
+    }
+
+    private void applyProcessor(final CtModel model) {
         FirstMethodProcessor firstMethodProcessor = new FirstMethodProcessor();
         MethodProcessor methodProcessor = new MethodProcessor();
         model.processWith(firstMethodProcessor);
         model.processWith(methodProcessor);
-        System.out.println("Modifiers present in project: " + methodProcessor.getAllMethodModifiersInProject());
+        System.out.println("Modifiers present in project: " +
+            methodProcessor.getAllMethodModifiersInProject());
         // System.out.println("Candidate methods to check for purity: ");
         // methodProcessor.getCandidateMethods().forEach(ctMethod -> System.out.println(ctMethod.getPath()));
-        System.out.println("Number of candidate methods to check for purity: " + methodProcessor.getCandidateMethods().size());
+        System.out.println("Number of candidate methods to check for purity: " +
+            methodProcessor.getCandidateMethods().size());
+    }
 
-        // Save model in spooned/
-        launcher.prettyprint();
+    private CtModel buildSpoonModel(final MavenLauncher launcher) {
+        launcher.buildModel();
+        return launcher.getModel();
+    }
+
+    private MavenLauncher getMavenLauncher(final String projectPath, final String projectName) {
+        MavenLauncher launcher = new MavenLauncher(projectPath, MavenLauncher.SOURCE_TYPE.APP_SOURCE);
+        launcher.getEnvironment().setAutoImports(true);
+        launcher.getEnvironment().setCommentEnabled(true);
+        return launcher;
+    }
+
+    private int countMethods(final CtModel model) {
+        int numberOfMethodsInProject = 0;
+        for (CtType<?> s : model.getAllTypes()) numberOfMethodsInProject += s.getMethods().size();
+        return numberOfMethodsInProject;
+    }
+
+    public Path getProjectPath() {
+        return projectPath;
     }
 }
--- src/main/java/processors/MethodProcessor.java
+++ src/main/java/processors/MethodProcessor.java
@@ -1,38 +1,47 @@
 package processors;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
 import spoon.processing.AbstractProcessor;
-import spoon.reflect.code.*;
+import spoon.reflect.code.CtAssignment;
+import spoon.reflect.code.CtBlock;
+import spoon.reflect.code.CtConstructorCall;
+import spoon.reflect.code.CtInvocation;
+import spoon.reflect.code.CtThrow;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.ModifierKind;
 import spoon.reflect.visitor.filter.TypeFilter;
 
-import java.util.*;
+@CommandLine.Command(
+    name = "methodsDescription",
+    description = "Process methods"
+)
+public class MethodProcessor extends AbstractProcessor<CtMethod<?>> implements Callable<String> {
 
-public class MethodProcessor extends AbstractProcessor<CtMethod<?>> {
     List<CtMethod> publicMethods = new ArrayList<>();
     List<CtMethod> privateMethods = new ArrayList<>();
     List<CtMethod> protectedMethods = new ArrayList<>();
     List<CtMethod> abstractMethods = new ArrayList<>();
     List<CtMethod> staticMethods = new ArrayList<>();
     List<CtMethod> synchronizedMethods = new ArrayList<>();
-    List<CtMethod> methodsThrowingExcepions = new ArrayList<>();
-
+    List<CtMethod> methodsThrowingExceptions = new ArrayList<>();
     List<CtMethod> emptyMethods = new ArrayList<>();
-
     Set<ModifierKind> allMethodModifiers = new HashSet<>();
-
     Set<CtMethod> candidateMethods = new HashSet<>();
 
-    public List<CtMethod> getEmptyMethods() {
-        return emptyMethods;
+    @Override
+    public String call() {
+        return toString();
     }
 
     public Set<ModifierKind> getAllMethodModifiersInProject() {
         return allMethodModifiers;
-    }
-
-    public List<CtMethod> getMethodsThrowingExcepions() {
-        return methodsThrowingExcepions;
     }
 
     // Find if method has no statements
@@ -68,7 +77,7 @@ public class MethodProcessor extends AbstractProcessor<CtMethod<?>> {
     // Find if method throws exceptions
     public boolean throwsExceptions(CtMethod ctMethod) {
         if (ctMethod.getThrownTypes().size() > 0 || (ctMethod.getBody().getElements(new TypeFilter<>(CtThrow.class)).size() > 0)) {
-            methodsThrowingExcepions.add(ctMethod);
+            methodsThrowingExceptions.add(ctMethod);
             return true;
         }
         return false;
@@ -100,8 +109,25 @@ public class MethodProcessor extends AbstractProcessor<CtMethod<?>> {
         // If method is not empty, abstract, or synchronized
         if (!(methodModifiers.contains(ModifierKind.ABSTRACT) || methodModifiers.contains(ModifierKind.SYNCHRONIZED) || isMethodEmpty(ctMethod))) {
             // and does not throw exceptions, invoke other methods, or has assignment statements, it is a candidate
-            if (!(throwsExceptions(ctMethod) || hasInvocations(ctMethod) || hasAssignments(ctMethod) || hasConstructorCalls(ctMethod)))
+            if (!(throwsExceptions(ctMethod) || hasInvocations(ctMethod) || hasAssignments(ctMethod) || hasConstructorCalls(ctMethod))) {
                 candidateMethods.add(ctMethod);
+            }
         }
+    }
+
+    @Override
+    public String toString() {
+        return "MethodProcessor{" +
+            "publicMethods=" + publicMethods +
+            ", privateMethods=" + privateMethods +
+            ", protectedMethods=" + protectedMethods +
+            ", abstractMethods=" + abstractMethods +
+            ", staticMethods=" + staticMethods +
+            ", synchronizedMethods=" + synchronizedMethods +
+            ", methodsThrowingExcepions=" + methodsThrowingExceptions +
+            ", emptyMethods=" + emptyMethods +
+            ", allMethodModifiers=" + allMethodModifiers +
+            ", candidateMethods=" + candidateMethods +
+            '}';
     }
 }
