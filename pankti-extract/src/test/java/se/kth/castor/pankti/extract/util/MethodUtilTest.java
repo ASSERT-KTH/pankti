d9c9d58f8c1aa555b3df02b668904152edbea4b0
Deepika Tiwari <deepikatiwari92@gmail.com>
2021-03-01 17:24:42 +0100

    #43: Update tests with correct number of mocking candidates

--- pankti-extract/src/test/java/se/kth/castor/pankti/extract/util/MethodUtilTest.java
+++ pankti-extract/src/test/java/se/kth/castor/pankti/extract/util/MethodUtilTest.java
@@ -12,7 +12,10 @@ import spoon.reflect.declaration.CtMethod;
 
 import java.net.URISyntaxException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -23,8 +26,11 @@ public class MethodUtilTest {
     static CtModel testModel;
     static MethodProcessor methodProcessor;
     static CandidateTagger candidateTagger;
-    static final String methodPath =
+    static final String methodPath1 =
             "#subPackage[name=org]#subPackage[name=jitsi]#subPackage[name=videobridge]#containedType[name=Conference]#method[signature=getDebugState(boolean,java.lang.String)]";
+    static final String methodPath2 =
+            "#subPackage[name=org]#subPackage[name=jitsi]#subPackage[name=videobridge]#containedType[name=Endpoint]#method[signature=getDebugState()]";
+    static List <CtMethod<?>> allMethods;
 
     @BeforeAll
     public static void setUpLauncherAndModel() throws URISyntaxException {
@@ -38,20 +44,51 @@ public class MethodUtilTest {
         panktiLauncher.addMetaDataToCandidateMethods(methodProcessor.getCandidateMethods());
         candidateTagger = new CandidateTagger();
         testModel.processWith(candidateTagger);
+        allMethods = getListOfMethods();
     }
 
-    // Test that nested method invocations that can be mocked are identified
-    @Test
-    public void testMockCandidatesAreMarked() {
+    private static CtMethod<?> findMethodByPath(final String path) {
         Optional<CtMethod<?>> optionalCtMethod =
                 candidateTagger
                         .getAllMethodTags()
                         .keySet()
                         .stream()
-                        .filter(k -> k.getPath().toString().equals(methodPath))
+                        .filter(k -> k.getPath().toString().equals(path))
                         .findFirst();
-        CtMethod<?> method = optionalCtMethod.get();
-        assertEquals(2, MethodUtil.findNestedMethodCalls(method).size(),
+        return optionalCtMethod.get();
+    }
+
+    private static List<String> getAllExtractedMethodPathsAsString() {
+        return candidateTagger
+                .getAllMethodTags()
+                .keySet()
+                .stream()
+                .map(k -> k.getPath().toString())
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private static List<CtMethod<?>> getListOfMethods() {
+        List<CtMethod<?>> allMethods = new ArrayList<>();
+        for (String methodPath : getAllExtractedMethodPathsAsString()) {
+            allMethods.add(findMethodByPath(methodPath));
+        }
+        return allMethods;
+    }
+
+    // Test that nested method invocations in methodPath1 that can be mocked are identified
+    @Test
+    public void testMockCandidatesAreMarked1() {
+        CtMethod<?> method = findMethodByPath(methodPath1);
+        assertEquals(0, MethodUtil.getNestedMethodInvocationMap(method).size(),
+                String.format("%s has no nested method invocations that may be mocked",
+                        method.getSignature()));
+    }
+
+    // Test that nested method invocations in methodPath2 that can be mocked are identified
+    @Test
+    public void testMockCandidatesAreMarked2() {
+        CtMethod<?> method = findMethodByPath(methodPath2);
+        assertEquals(2, MethodUtil.getNestedMethodInvocationMap(method).size(),
                 String.format("%s has two nested method invocations that may be mocked",
                         method.getSignature()));
     }
