28d0d0cf42649d54815fbaeac35c01340012e579
Deepika Tiwari <deepikatiwari92@gmail.com>
2021-10-20 19:22:56 +0200

    Add script + template to instrument nested invocations

---
+++ pankti-instrument/instrument-mock.py
@@ -0,0 +1,191 @@
+import glob
+import numpy as np
+import pandas as pd
+import re
+import sys
+
+def extract_from_nested_invocation_map(invocation_map):
+  nested_invocations = []
+  invocation_map_to_list = invocation_map.split(", ")
+  for invocation in invocation_map_to_list:
+    invocation = re.sub(r"(.+=)(.+)", r"\2", invocation)
+    nested_invocations.append(invocation.replace("}", "").replace("{", ""))
+  print("Found nested invocations", len(nested_invocations))
+  return nested_invocations
+
+def get_class_name_from_invocation_string(invocation):
+  return re.sub(r"(.+)(\.[a-zA-Z0-9]+\(.+)", r"\1", invocation)
+
+def get_method_from_invocation_string(invocation):
+  method_and_args = re.sub(r"(.+)(\.[a-zA-Z0-9]+\(.+)", r"\2", invocation)
+  method_and_args = re.sub(r"^\.", "", method_and_args)
+  return re.sub(r"(.+)(\(.+)", r"\1", method_and_args)
+
+def get_parameters_from_invocation_string(invocation):
+  method_and_args = re.sub(r"(.+)(\.[a-zA-Z0-9]+\(.+)", r"\2", invocation)
+  return re.sub(r"(.+)(\(.+)", r"\2", method_and_args)
+
+def sanitize_parameter_list(param_list):
+  if param_list.startswith("[") and param_list.endswith("]"):
+    param_list = re.sub(r"^\[", "", param_list)
+    param_list = re.sub(r"\]$", "", param_list)
+  parameters = param_list.replace("(", "").replace(")", "")
+  parameters = re.sub(r"([a-zA-Z0-9\$\.\[\]]+)", "\"" + r"\1" + "\"", parameters)
+  parameters = re.sub("\"T\"", "\"java.lang.Object\"", parameters)
+  parameters = re.sub("\"T\[\]\"", "\"java.lang.Object[]\"", parameters)
+  return parameters
+
+# Generate aspect classes based on the MethodAspect0Nested0 template
+def generate_mock_aspect_class(template_file_path, new_file_path, count, nested_count, nested_invocation):
+  print("Generating aspect class", new_file_path)
+  class_name = get_class_name_from_invocation_string(nested_invocation)
+  method_name = get_method_from_invocation_string(nested_invocation)
+  param_list = get_parameters_from_invocation_string(nested_invocation)
+  with open(template_file_path) as template:
+    with open(new_file_path, "w") as f:
+      for line in template:
+        if ("public class MethodAspect0Nested0" in line):
+          line = line.replace("Aspect0", "Aspect" + str(count))
+          line = line.replace("Nested0", "Nested" + str(nested_count))
+        if ("@Pointcut(className =" in line):
+          line = re.sub(r"=\s\"(.+)\",", "= \"" + class_name + "\",", line)
+        if ("methodName = " in line):
+          line = re.sub(r"=\s\"(.+)\",", "= \"" + method_name + "\",", line)
+        if ("methodParameterTypes = " in line):
+          if (param_list == "()"):
+            param_list = ""
+          else:
+            param_list = sanitize_parameter_list(param_list)
+          line = re.sub(r"=\s{.*},", "= {" + param_list + "},", line)
+        if ("timerName = " in line):
+          line = re.sub(r"=\s\".+\"\)", "= \"" + class_name + "-" + method_name + "\")", line)
+        if ("double COUNT = " in line):
+          line = re.sub(r"=\s\d+;", "= " + str(count) + "." + str(nested_count) + ";", line)
+        if ("MethodAspect0.TargetMethodAdvice" in line):
+          line = line.replace("MethodAspect0", "MethodAspect" + str(count))
+        f.write(line)        
+
+# Generate aspect classes based on the MethodAspect0 template
+def generate_aspect_class(template_file_path, new_file_path, count, row, df):
+  print("Generating aspect class", new_file_path)
+  with open(template_file_path) as template:
+    with open(new_file_path, "w") as f:
+      for line in template:
+        if ("public class MethodAspect0" in line):
+          line = line.replace("0", str(count))
+        if ("@Pointcut(className =" in line):
+          line = re.sub(r"=\s\"(.+)\",", "= \"" + row['parent-FQN'] + "\",", line)
+        if ("methodName = " in line):
+          line = re.sub(r"=\s\"(.+)\",", "= \"" + row['method-name'] + "\",", line)
+        if ("String rowInCSVFile" in line):
+          values = []
+          for col in df.columns:
+            col_value = str(row[col])
+            if "," in col_value:
+              col_value = "\\\"" + col_value.replace(" ", "") + "\\\""
+            values.append(col_value)
+          row_as_string = ','.join(values)
+          line = re.sub(r"\"\"", "\"" + row_as_string + "\"", line)
+        # Changes needed for void methods
+        if row['return-type'] == "void":
+          if "boolean isReturnTypeVoid" in line:
+            line = line.replace("false", "true")
+            # if "@OnReturn" in line:
+          if "onReturn(@BindReturn Object returnedObject," in line:
+            line = line.replace("@BindReturn Object returnedObject", "@BindReceiver Object receivingObjectPost")
+          if "writeObjectXMLToFile(returnedObject, returnedObjectFilePath)" in line:
+            line = line.replace("returnedObject", "receivingObjectPost")
+        if ("methodParameterTypes = " in line):
+          if (pd.isnull(row['param-list'])):
+            param_list = ""
+          else:
+            param_list = sanitize_parameter_list(row['param-list'])
+          line = re.sub(r"=\s{.*},", "= {" + param_list + "},", line)
+        if ("timerName = " in line):
+          line = re.sub(r"=\s\".+\"\)", "= \"" + row['parent-FQN'] + "-" + row['method-name'] + "\")", line)
+        if ("int COUNT = " in line):
+          line = re.sub(r"=\s\d+;", "= " + str(count) + ";", line)
+        f.write(line)
+
+# Generate aspect classes
+def generate_aspects(df):
+  base_path = "./src/main/java/se/kth/castor/pankti/instrument/plugins/MethodAspect"
+  found_aspects = sorted(glob.glob(base_path + "*.java"), key=lambda x:float(re.findall("(\d+)",x)[0]))
+  count = int(re.search(r"(\d+)", found_aspects[-1]).group())
+  aspects = []
+  template_file_path = base_path + str(0) + ".java"
+  mock_template_file_path = base_path + "0Nested0.java" 
+  df.replace(np.nan, '', regex=True, inplace=True)
+  for index, row in df.iterrows():
+  # temporarily, instrument classes with mockable invocations, with non-Java library nested invocations
+    if row['has-mockable-invocations'] and "java." not in row['nested-invocations']:
+      count += 1
+      aspects.append(float(count))
+      new_file_path = base_path + str(count) + ".java"
+      generate_aspect_class(template_file_path, new_file_path, count, row, df)
+      if (row['has-mockable-invocations']):
+        nested_invocations = extract_from_nested_invocation_map(row['nested-invocations'])
+        for i in range(len(nested_invocations)):
+          nested_count = i + 1
+          new_file_path = base_path + str(count) + "Nested" + str(nested_count) + ".java"
+          aspects.append(float(str(count) + "." + str(nested_count)))
+          generate_mock_aspect_class(mock_template_file_path, new_file_path, count, nested_count, nested_invocations[i])
+  print("New aspect classes added in se.kth.castor.pankti.instrument.plugins")
+  return aspects
+
+# Update Glowroot plugin
+def update_glowroot_plugin_json(aspects):
+  aspect_path = "se.kth.castor.pankti.instrument.plugins.MethodAspect"
+  plugin_json_path = "./src/main/resources/META-INF/glowroot.plugin.json"
+  index = 0
+  aspect_lines = ""
+  has_existing_aspects = False
+  last_existing_aspect = ""
+  for i in range(len(aspects)):
+    whole = re.sub(r"(\d+)\.(\d+)", r"\1", str(aspects[i]))
+    frac = re.sub(r"(\d+)\.(\d+)", r"\2", str(aspects[i]))
+    if frac == '0':
+      aspect_lines += "    \"" + aspect_path + whole + "\""
+    else:
+      aspect_lines += "    \"" + aspect_path + whole + "Nested" + frac + "\""
+    if i < len(aspects) - 1:
+      aspect_lines += ","
+    aspect_lines += "\n"
+  with open(plugin_json_path, "r+") as json_file:
+    for num, line in enumerate(json_file, 1):
+      if "aspects" in line:
+        index = num
+      if re.search(r"MethodAspect.+\"[^,]", line):
+        index = num - 1
+        has_existing_aspects = True
+        last_existing_aspect = line
+  if has_existing_aspects:
+    with open(plugin_json_path, "r+") as json_file:
+      contents = json_file.readlines()
+      json_file.seek(0)
+      for line in contents:
+        if line != last_existing_aspect:
+          json_file.write(line)
+      json_file.truncate()
+  with open(plugin_json_path, "r+") as json_file:
+    contents = json_file.readlines()
+    if has_existing_aspects:
+      aspect_lines = last_existing_aspect.replace("\n", ",\n") + aspect_lines
+    contents.insert(index, aspect_lines)
+    json_file.seek(0)
+    json_file.writelines(contents)
+  print("resources/META-INF/glowroot.plugin.json updated")
+
+def main(argv):
+  try:
+    df = pd.read_csv(argv[1])
+    print("input (rows, columns): ", df.shape)
+    aspects = generate_aspects(df)
+    update_glowroot_plugin_json(aspects)
+  except Exception as e:
+    print("USAGE: python instrument-mock.py </path/to/instrumentation/candidate/list>.csv")
+    print(e)
+    sys.exit()
+
+if __name__ == "__main__":
+  main(sys.argv)
---
+++ pankti-instrument/src/main/java/se/kth/castor/pankti/instrument/plugins/MethodAspect0Nested0.java
@@ -0,0 +1,216 @@
+package se.kth.castor.pankti.instrument.plugins;
+
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import org.glowroot.agent.plugin.api.*;
+import org.glowroot.agent.plugin.api.weaving.*;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MethodAspect0Nested0 {
+    private static int INVOCATION_COUNT;
+    private static boolean fileSizeWithinLimits = true;
+
+    @Pointcut(className = "fully.qualified.path.to.class",
+            methodName = "methodToInstrument",
+            methodParameterTypes = {"param1", "param2"},
+            timerName = "Timer - name")
+    public static class TargetMethodAdvice implements AdviceTemplate {
+        private static final TimerName timer = Agent.getTimerName(TargetMethodAdvice.class);
+        private static final String transactionType = "Target";
+        private static final double COUNT = 0;
+        private static long profileSizePre;
+        private static String paramObjectsFilePath;
+        private static String returnedObjectFilePath;
+        private static String invocationCountFilePath;
+        private static String invokedMethodsCSVFilePath;
+        private static String objectProfileSizeFilePath;
+        private static Logger logger = Logger.getLogger(TargetMethodAdvice.class);
+        private static final String methodParamTypesString = String.join(",", TargetMethodAdvice.class.getAnnotation(Pointcut.class).methodParameterTypes());
+        private static final String postfix = methodParamTypesString.isEmpty() ? "" : "_" + methodParamTypesString;
+        private static final String methodFQN = TargetMethodAdvice.class.getAnnotation(Pointcut.class).className() + "."
+                + TargetMethodAdvice.class.getAnnotation(Pointcut.class).methodName() + postfix;
+        private static final String invocationString = String.format("Invocation count for %s: ", methodFQN);
+        private static File[] allObjectFiles;
+
+        private static void setup() {
+            AdviceTemplate.setUpXStream();
+            Map<Type, String> fileNameMap = AdviceTemplate.setUpFiles("nested-" + methodFQN);
+            paramObjectsFilePath = fileNameMap.get(Type.PARAMS);
+            returnedObjectFilePath = fileNameMap.get(Type.RETURNED);
+            invocationCountFilePath = fileNameMap.get(Type.INVOCATION_COUNT);
+            invokedMethodsCSVFilePath = fileNameMap.get(Type.INVOKED_METHODS);
+            objectProfileSizeFilePath = fileNameMap.get(Type.OBJECT_PROFILE_SIZE);
+            allObjectFiles = new File[]{
+                    new File(returnedObjectFilePath),
+                    new File(paramObjectsFilePath)};
+            checkFileSizeLimit();
+        }
+
+        public static long getObjectProfileSize() {
+            long objectProfileSize = 0L;
+            for (File file : allObjectFiles) {
+                objectProfileSize += file.length();
+            }
+            return objectProfileSize;
+        }
+
+        // Limit object XML files to ~200 MB
+        public static void checkFileSizeLimit() {
+            for (File file : allObjectFiles) {
+                if (file.exists() && (file.length() / (1024 * 1024) >= 200)) {
+                    fileSizeWithinLimits = false;
+                    break;
+                }
+            }
+        }
+
+        public static synchronized void writeObjectXMLToFile(Object objectToWrite, String objectFilePath) {
+            try {
+                FileWriter objectFileWriter = new FileWriter(objectFilePath, true);
+                String xml = xStream.toXML(objectToWrite);
+                xml = xml.replaceAll("(&#x)(\\w+;)", "&amp;#x$2");
+                BufferedReader reader = new BufferedReader(new StringReader(xml));
+                BufferedWriter writer = new BufferedWriter(objectFileWriter);
+                while ((xml = reader.readLine()) != null) {
+                    writer.write(xml);
+                    writer.newLine();
+                }
+                writer.flush();
+                writer.close();
+            } catch (Exception e) {
+                logger.info("Exception when writing XML for MethodAspect" + COUNT + " to file " + objectFilePath);
+                e.printStackTrace();
+                if (e.getMessage().startsWith("No converter specified") || e.getMessage().startsWith("No converter available")) {
+                    String className = extractClassNameFromTheExceptionMessage(e.getMessage());
+                    registerConverterAtRuntime(className);
+                    logger.info("Automatically register a converter for: " + className);
+                }
+            }
+        }
+
+        // Write size (in bytes) of individual object profile to file
+        public static synchronized void writeObjectProfileSizeToFile(long size) {
+            try {
+                FileWriter objectFileWriter = new FileWriter(objectProfileSizeFilePath, true);
+                objectFileWriter.write(size + "\n");
+                objectFileWriter.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public static synchronized void writeInvocationCountToFile() {
+            try {
+                File invocationCountFile = new File(invocationCountFilePath);
+
+                if (!invocationCountFile.exists()) {
+                    FileWriter objectFileWriter = new FileWriter(invocationCountFilePath);
+                    objectFileWriter.write(invocationString + INVOCATION_COUNT);
+                    objectFileWriter.close();
+                } else {
+                    String content = Files.readString(Paths.get(invocationCountFilePath));
+                    Pattern pattern = Pattern.compile(invocationString + "(\\d+$)");
+                    Matcher matcher = pattern.matcher(content);
+                    if (matcher.find()) {
+                        int count = Integer.parseInt(matcher.group(1)) + 1;
+                        content = matcher.replaceAll(invocationString + count);
+                        Files.write(invocationCountFile.toPath(), content.getBytes(StandardCharsets.UTF_8));
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        // For mocking: instrument and collect parameters and returned values if this invocation is nested
+        @IsEnabled
+        public static boolean isNestedInvocation() {
+            String parentInvocationClassName = MethodAspect0.TargetMethodAdvice.class.getAnnotation(Pointcut.class).className();
+            String parentInvocationMethodName = MethodAspect0.TargetMethodAdvice.class.getAnnotation(Pointcut.class).methodName();
+            boolean isNestedInvocation = Arrays.stream(Thread.currentThread().getStackTrace()).anyMatch(s ->
+                    s.getClassName().equals(parentInvocationClassName) &
+                            s.getMethodName().equals(parentInvocationMethodName));
+            if (isNestedInvocation)
+                logger.info("Aspect " + COUNT + " is a nested invocation");
+            return isNestedInvocation;
+        }
+
+        @OnBefore
+        public static TraceEntry onBefore(OptionalThreadContext context,
+                                          @BindParameterArray Object parameterObjects,
+                                          @BindMethodName String methodName) {
+            setup();
+            if (fileSizeWithinLimits) {
+                profileSizePre = getObjectProfileSize();
+                writeObjectXMLToFile(parameterObjects, paramObjectsFilePath);
+            }
+            MessageSupplier messageSupplier = MessageSupplier.create(
+                    "className: {}, methodName: {}",
+                    TargetMethodAdvice.class.getAnnotation(Pointcut.class).className(),
+                    methodName
+            );
+            return context.startTransaction(transactionType, methodName, messageSupplier, timer, OptionalThreadContext.AlreadyInTransactionBehavior.CAPTURE_NEW_TRANSACTION);
+        }
+
+        @OnReturn
+        public static void onReturn(@BindReturn Object returnedObject,
+                                    @BindTraveler TraceEntry traceEntry) {
+            if (fileSizeWithinLimits) {
+                writeObjectXMLToFile(returnedObject, returnedObjectFilePath);
+                writeObjectProfileSizeToFile(getObjectProfileSize() - profileSizePre);
+                checkFileSizeLimit();
+            }
+            INVOCATION_COUNT++;
+            writeInvocationCountToFile();
+            traceEntry.end();
+        }
+
+        @OnThrow
+        public static void onThrow(@BindThrowable Throwable throwable,
+                                   @BindTraveler TraceEntry traceEntry) {
+            traceEntry.endWithError(throwable);
+        }
+
+        public static String extractClassNameFromTheExceptionMessage(String exceptionMessage) {
+            String className = "";
+
+            String pattern = "type\\s+:\\s+(\\S*)\\n";
+            Pattern r = Pattern.compile(pattern);
+            Matcher m = r.matcher(exceptionMessage);
+            if (m.find()) {
+                className = m.group(1);
+            }
+
+            return className;
+        }
+
+        public static void registerConverterAtRuntime(String className) {
+            xStream.registerConverter(new Converter() {
+                @Override
+                public void marshal(Object o, HierarchicalStreamWriter hierarchicalStreamWriter, MarshallingContext marshallingContext) {
+                }
+
+                @Override
+                public Object unmarshal(HierarchicalStreamReader hierarchicalStreamReader, UnmarshallingContext unmarshallingContext) {
+                    return null;
+                }
+
+                @Override
+                public boolean canConvert(Class aClass) {
+                    return aClass.getCanonicalName().equals(className);
+                }
+            });
+        }
+    }
+}
